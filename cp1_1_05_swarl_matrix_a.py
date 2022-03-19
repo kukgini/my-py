a46c95c3273523b0e68eda6a2dc488ed870a88d1
kukgini <kukgini@gmail.com>
2022-03-19 17:06:32 +0900

    ...

--- cp1_1_05_swarl_matrix_a.py
+++ cp1_1_05_swarl_matrix_a.py
@@ -4,11 +4,10 @@ def print_matrix(matrix):
 
 def make_matrix(n):
     m = [[0] * n for _ in range(n)]
-    print_matrix(m)
+    # print_matrix(m)
     return m
 
 def solution(n):
-    # return cp1_1_05_swarl_matrix_a.solution(n)
     matrix = make_matrix(n)
     distance = n
     row, col = 0, -1
@@ -25,11 +24,16 @@ def solution(n):
         dir = (dir + 1) % 4 # 0,1,2,3 % 4개 방향으로 진행 방향을 바꾼다.
         if divmod(dir,2)[1]: # 2로 나누어 떨어지지 않으면 홀수
             distance -= 1 # col, row 를 한번씩 진행 했으면 1 step 줄인다.
-    # print_matrix(matrix)
+    print_matrix(matrix)
     answer = 0
     for x in range(n):
         answer += matrix[x][x] # 대각선의 합을 구한다.
     return answer
 
-if __name__  == "__main__":
-    print(f'answer={solution(3)}')
+if __name__ == "__main__":
+    input = 2
+    print("input=", input, "result=", solution(input));
+    input = 3
+    print("input=", input, "result=", solution(input));
+    input = 4
+    print("input=", input, "result=", solution(input));
