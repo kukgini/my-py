6d8dca10c5291670e564014f5d2cf67b17fc2c6e
kukgini <kukgini@gmail.com>
2022-03-19 13:11:23 +0900

    ...

--- cp1-1-4.py
+++ cp1-1-4.py
@@ -1,7 +1,7 @@
+# solution 1)
 # number -> string -> list 변환
 # list -> string - int 변환
-
-def solution(num):
+def solution1(num):
     num += 1
     n = list(str(num))
     for i, v in enumerate(n):
@@ -9,7 +9,13 @@ def solution(num):
             n[i] = "1"
     return int("".join(n))
 
+# solution 2)
+# comprehension
+def solution2(num):
+    return ''.join('1' if x == '0' else x for x in str(num))
+
+
 if __name__ == "__main__":
     input = 9949999
-    ret = solution(input)
-    print("result=", ret);
+    ret = solution2(input)
+    print("solution2 result=", ret);
