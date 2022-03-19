5a941d54034067c4ef36c9e2058d9d1f53411045
kukgini <kukgini@gmail.com>
2022-03-19 17:35:53 +0900

    ...

---
+++ cp1_1_07_merge_sort.py
@@ -0,0 +1,9 @@
+import cp1_1_07_merge_sort_a
+
+def solution(a,b):
+    return cp1_1_07_merge_sort_a.solution(a,b)
+
+if __name__ == "__main__":
+    a = [-2, 3, 5, 9]
+    b = [0, 1, 5]
+    print(f'answer={solution(a,b)}')
