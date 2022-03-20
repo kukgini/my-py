99fae13c931ec6f15edeba7a32980bade9ac5171
kukgini <kukgini@gmail.com>
2022-03-20 10:16:28 +0900

    ..

---
+++ c1_1_09_step_game_a.py
@@ -0,0 +1,26 @@
+def func(record):
+    if record == 0:
+        return 1
+    elif record == 1:
+        return 2
+    return 0
+
+def solution(a,b):
+    cnt = 0 # A 의 계단 위치
+    results = []
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            a.append(cnt)
+            continue
+        elif a[i] == func(b[i]):
+            cnt = cnt + 3
+        else:
+            # cnt = cnt - 1
+            cnt = 0 if cnt == 0 else (cnt - 1)
+        results.append(cnt);
+    print(f'r={results}')
+    return cnt
+
+if __name__ == "__main__":
+    a, b = [2,0,0,0,0,0,1,1,0,0], [0,0,0,0,2,2,0,2,2,2]
+    print(f'a={a}\nb={b}\nanswer={solution(a,b)}')
