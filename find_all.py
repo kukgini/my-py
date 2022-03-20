2f99598a3fb67ac8aa45ea1da5bf64d569c13790
kukgini <kukgini@gmail.com>
2022-03-20 10:41:49 +0900

    no message

--- find_all.py
+++ find_all.py
@@ -1,2 +1,6 @@
 def find_all(string, pattern):
     return len([i for i in range(len(string)) if string.startswith(pattern, i)])
+
+if __name__ == "__main__":
+    string, pattern = "aBCBCaaaefBCgBC", "BC"
+    print(f'string={string}, pattern={pattern}, answer={find_all(string,pattern)}')
