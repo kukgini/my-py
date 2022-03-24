609ed10d110740fcc5d8f26b5a3a14812bb09361
kukgini <kukgini@gmail.com>
2022-03-24 16:56:11 +0900

    no message

--- cp1_1_06_chess_night_a.py
+++ cp1_1_06_chess_night_a.py
@@ -5,8 +5,13 @@ def solution(pos):
     # r = ord(pos[1]) - ord('1') # ascii code of 1 = 48
 
     # 방법2. 좌표 map 사용
-    xcords = dict([("A", 0), ("B", 1), ("C", 2), ("D", 3), ("E", 4), ("F", 5), ("G", 6), ("H", 7)])
-    ycords = dict([("1", 0), ("2", 1), ("3", 2), ("4", 3), ("5", 4), ("6", 5), ("7", 6), ("6", 7)])
+    # xcords = dict([("A",0), ("B",1), ("C",2), ("D",3), ("E",4), ("F",5), ("G",6), ("H",7)])
+    # ycords = dict([("1",0), ("2",1), ("3",2), ("4",3), ("5",4), ("6",5), ("7",6), ("8",7)])
+
+    # 방법3. 좌표 map 을 zip 으로 생성하여 사용
+    xcords = dict(zip("ABCDEFGH",[i for i in range(8)]))
+    ycords = dict(zip("12345678",[i for i in range(8)]))
+
     c = xcords[pos[0]]
     r = ycords[pos[1]]
 
